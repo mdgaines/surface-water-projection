aa961736b945cbec9c0566ed817b795226bc7c8b
Mollie Gaines <mdgaines.22@gmail.com>
2022-12-08 23:29:37 -0500

    mk_year_season_csv working for lclu, fore-sce_processing updated for new study area

--- calc_error_dist.py
+++ calc_error_dist.py
@@ -36,31 +36,53 @@ def mk_year_season_csv(outpath = str):
         
         parameter:
             outpath: path to where the csv will be saved
-                '../data/ClimateData/macav2livneh_GRIDMET_CSVs/{src}/{src}_{szn}_{var}_AVG_{scn (opitonal)}_{tag}.csv'
+                climate: '../data/ClimateData/macav2livneh_GRIDMET_CSVs/{src}/{src}_{szn}_{var}_AVG_{scn (opitonal)}_{tag}.csv'
+                lclu   : '../data/LandCover/FORESCE_NLCDCDL_CSVs/{scn}/{src}_{var}_FRAC_{scn}_{tag}.csv'
     '''
+    var_group = outpath.split('/')[2]
     info_lst = os.path.basename(outpath)[:-4].split('_')
-    src = info_lst[0] # data source (GRIDMET, GFDL, etc.)
-    szn = info_lst[1] # season (Sp, Su, Fa, Wi for GRIDEMT; SPRING, SUMMER, FALL, WINTER for GCMs)
-    if 'TEMP' in outpath:
-        var = '_'.join([info_lst[2], info_lst[3]])
-    else:
-        var = info_lst[2] # variable (Pr, maxTemp, minTemp for GRIDMET; PRECIP, MAX_TEMP, MIN_TEMP for GCMs)
-    scn = info_lst[-2] # scenario (HISTORICAL, RCP45, RCP85 for GCMs)
+    src = info_lst[0] # data source (GRIDMET, GFDL, etc.; FORESCE, NLCDCDL for LCLU)
+    scn = info_lst[-2] # scenario (HISTORICAL, RCP45, RCP85 for GCMs; A1B, A2, B1, B2 for LCLU)
 
-    if src=='GRIDMET':
-        fl_lst = glob(f'../data/ClimateData/GRIDMET_AVG_STDEV/GRIDMET_YR_AVG/*{szn}_{var}*.csv')
-    else:
-        fl_lst = glob(f'../data/ClimateData/{src}*/{scn}/zonal_avg/*{szn}_{var}*.csv')
+    if var_group == 'ClimateData':
+        szn = info_lst[1] # season (Sp, Su, Fa, Wi for GRIDEMT; SPRING, SUMMER, FALL, WINTER for GCMs)
+        if 'TEMP' in outpath:
+            var = '_'.join([info_lst[2], info_lst[3]])
+        else:
+            var = info_lst[2] # variable (Pr, maxTemp, minTemp for GRIDMET; PRECIP, MAX_TEMP, MIN_TEMP for GCMs)
+
+        if src=='GRIDMET':
+            fl_lst = glob(f'../data/ClimateData/GRIDMET_AVG_STDEV/GRIDMET_YR_AVG/*{szn}_{var}*.csv')
+        else:
+            fl_lst = glob(f'../data/ClimateData/{src}*/{scn}/zonal_avg/*{szn}_{var}*.csv')
+    
+    else: # LandCover
+        var_dict = {'AGRI': ['AG', 'agro'],\
+                    'INTS': ['INT', 'intense'],\
+                    'FRST': ['NAT', 'natural']}
+        if src == 'FORESCE':
+            var = var_dict[info_lst[1]][0]
+            fl_lst = glob(f'../data/LandCover/FORE-SCE/*{scn}/zonal_lclu_csv/*.csv')
+        else:
+            var = var_dict[info_lst[1]][1]
+            fl_lst = glob(f'../data/LandCover/CDL_paper2/*{var}*.csv')
 
     # loop through files and get climate var for each year
     for i in range(len(fl_lst)):
         fl = pd.read_csv(fl_lst[i])
         if src == 'GRIDMET':
-            col_name= 'mean'
+            col_name = 'mean'
             yr = fl['Yr_Szn'][0][:4]
-        else:
+        elif src != 'GRDIMET' and var_group == 'ClimateData':
             col_name = f'AVG_{var}'
             yr = fl['YEAR'][0]
+        elif src == 'FORESCE':
+            col_name = f'PR_{var}'
+            yr = str(fl['YEAR'][0])
+        elif src == 'NLCDCDL':
+            fl['PR_LC'] = (fl['sum'] * 0.0009 ) / fl['areasqkm'] # get percent LCLU
+            col_name = 'PR_LC'
+            yr = fl['Yr_Szn'][0][:4]
 
         if i == 0:
             full_df = fl[['huc8', col_name]]
@@ -69,6 +91,8 @@ def mk_year_season_csv(outpath = str):
             full_df = full_df.join(fl[['huc8', col_name]].set_index('huc8'), on='huc8')
             full_df = full_df.rename(columns={col_name:yr})
 
+    if var_group == 'LandCover':
+        full_df = full_df.reindex(sorted(full_df.columns), axis=1)
     # save CSV
     full_df.to_csv(outpath)
     print(f'{os.path.basename(outpath)} saved.')
@@ -205,12 +229,14 @@ def calc_error_correlation(precip_path=str, mxTemp_path=str, png_outpath=str, \
 #    - raw diff, mean percent error, median percent error
 #    - try to figure out which is best to use for MC bootstrapping
 # 3) plot error dist for each szn, huc4, variable
-# 4) 
-# 5) check for temporal trend in error
-# 6) fit distributions...
+# 4) check for correlation/calculate mu and sigma
+# 5) REPEAT for LCLU
+
+# ) check for temporal trend in error
+# New PY file) fit distributions...
 
 def main():
-    src_lst = ['GRIDMET', 'GFDL', 'HadGEM2', 'IPSL', 'MIROC5', 'NorESM1']
+    src_lst = ['GRIDMET', 'GFDL', 'HadGEM2', 'IPSL', 'MIROC5', 'NorESM1', 'FORESCE', 'NLCDCDL']
     obs_proj_dict = {'OBS': [['Sp', 'Su', 'Fa', 'Wi'], ['Pr', 'maxTemp', 'minTemp']],\
                      'PROJ': [['SPRING', 'SUMMER', 'FALL', 'WINTER'], ['PRECIP', 'MAX_TEMP', 'MIN_TEMP']]}
 
@@ -218,21 +244,33 @@ def main():
     #### make csvs ####
     ###############################################
     for src in src_lst:
-        if src == 'GRIDMET':
+        if src == 'GRIDMET' or src == 'NLCDCDL':
             tag = 'OBS'
             scn_lst = ['OBS']
+        elif src == 'FORESCE':
+            tag = 'PROJ'
+            scn_lst = ['A1B', 'A2', 'B1', 'B2']
         else:
             tag = 'PROJ'
             scn_lst = ['HISTORICAL_PROJ', 'RCP45_PROJ', 'RCP85_PROJ']
         szn_lst = obs_proj_dict[tag][0]
-        var_lst = obs_proj_dict[tag][1] 
+        var_lst = obs_proj_dict[tag][1]
 
         outpath_lst = [f'../data/ClimateData/macav2livneh_GRIDMET_CSVs/{src}/{src}_{szn}_{var}_AVG_{scn}.csv'\
                         for szn in szn_lst for var in var_lst for scn in scn_lst]
 
+        if src == 'FORESCE':
+            var_lst = ['AGRI', 'INTS', 'FRST']
+            outpath_lst = [f'../data/LandCover/FORESCE_NLCDCDL_CSVs/{scn}/{src}_{var}_FRAC_{scn}_{tag}.csv' \
+                            for var in var_lst for scn in scn_lst]
+        elif src == 'NLCDCDL':
+            var_lst = ['AGRI', 'INTS', 'FRST']
+            outpath_lst = [f'../data/LandCover/FORESCE_NLCDCDL_CSVs/{src}/{src}_{var}_FRAC_{tag}.csv' \
+                            for var in var_lst]
+
         for outpath in outpath_lst:
             if not os.path.exists(os.path.dirname(outpath)):
-                os.mkdir(os.path.dirname(outpath))
+                os.makedirs(os.path.dirname(outpath))
             if not os.path.exists(outpath):
                 mk_year_season_csv(outpath)
     print('All OBS and PROJ csv have been saved.')
@@ -285,6 +323,7 @@ def main():
 
             outpath = f'../imgs/Paper2/error_dist/{src}/{szn}/{var}/{scn}/{src}_{szn}_{var}_DIFF_{scn}_{huc4}_error_dist.png'    
             save_error_png(outpath, huc4_diff_path, dist=True)
+    print('All DIFF PNGs have been saved.')
     ###############################################
 
     ###############################################
@@ -308,7 +347,9 @@ def main():
                                    mxTemp_path=huc4_mxTemp_lst[i],
                                    png_outpath=png_outpath,
                                    src=src, szn=szn, scn=scn, huc4=huc4)
-    
+    print('All BIVAR JSONs and PNGs have been saved.')
+    ###############################################
+
 
 if __name__ == '__main__':
     main()
--- fore_sce_processing.py
+++ fore_sce_processing.py
@@ -18,6 +18,10 @@ import os
 ##### Clip to study area #####
 
 tif_paths = glob('../data/LandCover/FORE-SCE/CONUS_Landcover_*/*.tif')
+huc_outline = gpd.read_file('../data/Shapefiles/outline_paper2/outline_clean_paper2.shp')
+src = rxr.open_rasterio(tif_paths[0])
+huc_outline_reproj = huc_outline.to_crs(crs=src.rio.crs.to_dict()['init'])
+del(src)
 
 for i in range(len(tif_paths)):
     in_path = tif_paths[i]
@@ -26,27 +30,24 @@ for i in range(len(tif_paths)):
     sres = sres_path.split('_')[2]
     yr = in_path.split('_')[4][1:5]
 
-    out_path = os.path.join('../data/LandCover/FORE-SCE',sres_path,'clipped/{0}_{1}_clipped.tif'.format(yr, sres))
+    out_path = os.path.join('../data/LandCover/FORE-SCE',sres_path,f'clipped/{yr}_{sres}_clipped.tif')
+    if not os.path.exists(out_path):
+        if not os.path.exists(os.path.dirname(out_path)):
+            os.makedirs(os.path.dirname(out_path))
 
-    src = rxr.open_rasterio(tif_paths[i])
+        src = rxr.open_rasterio(tif_paths[i])
+       
+        clipped = src.rio.clip(huc_outline_reproj.geometry.apply(mapping),
+                                        # This is needed if your GDF is in a diff CRS than the raster data
+                                        huc_outline_reproj.crs, drop=True, invert=False)
 
-    if i == 0:
-        huc_outline = gpd.read_file('../data/Shapefiles/HUC08_outline_clean.shp')
-        huc_outline_reproj = huc_outline.to_crs(crs=src.rio.crs.to_dict())
+        clipped.rio.to_raster(out_path)
+        del(src)
+        del(clipped)
+        print('{0}/{1} clips saved. {2}_{3}_clipped.tif'.format(i+1, len(tif_paths), yr, sres))
 
-    if os.path.exists(out_path):
-        print('{0}_{1}_clipped.tif exists.'.format(yr, sres))
-        continue
-
-    clipped = src.rio.clip(huc_outline_reproj.geometry.apply(mapping),
-                                      # This is needed if your GDF is in a diff CRS than the raster data
-                                      huc_outline_reproj.crs)
-
-    clipped.rio.to_raster(out_path)
-    del(src)
-    del(clipped)
-    print('{0}/{1} clips saved. {2}_{3}_clipped.tif'.format(i+1, len(tif_paths), yr, sres))
-
+    else:
+        print(f'{yr}_{sres}_clipped.tif exists.')
 del(huc_outline, huc_outline_reproj)
 
 
@@ -58,43 +59,45 @@ tif_paths = glob('../data/LandCover/FORE-SCE/CONUS_Landcover_*/clipped/*.tif')
 for i in range(len(tif_paths)):
     in_path = tif_paths[i]
     yr, sres, tif = os.path.basename(in_path).split('_')
-    out_path = os.path.join(os.path.dirname(os.path.dirname(in_path)),'reclass/{0}_{1}_reclass.tif'.format(yr, sres))
+    out_path = os.path.join(os.path.dirname(os.path.dirname(in_path)),f'reclass/{yr}_{sres}_reclass.tif')
+    if not os.path.exists(out_path):
+        if not os.path.exists(os.path.dirname(out_path)):
+            os.makedirs(os.path.dirname(out_path))
 
-    if os.path.exists(out_path):
-        print('{0}_{1}_reclass.tif exists.'.format(yr, sres))
-        continue
+        with rio.open(in_path) as src:
+        # src = rio.open(in_path)
 
-    with rio.open(in_path) as src:
-    # src = rio.open(in_path)
+            src_data = src.read(1)
+            np_shape = (3, src_data.shape[0], src_data.shape[1])
+            suit_raster = np.zeros(np_shape, dtype=rio.int8)
+            # Reclassify to natural land cover
+            suit_raster[0] = np.select([src_data == 0, ((7 < src_data) & (src_data <= 12)) | ((13 < src_data) & (src_data <= 16)),\
+                                                        (src_data < 7) | (src_data == 13) | (src_data > 16)], [0,1,0])
+            # Reclassify to Agricultural land cover
+            suit_raster[1] = np.select([src_data == 0, (src_data == 13),\
+                                                        (src_data < 13) | (src_data > 13)], [0,1,0])
+            # Reclassify to Intensive
+            suit_raster[2] = np.select([src_data == 0, ((1 < src_data) & (src_data <= 7)),\
+                                                        (src_data < 1) | (src_data > 7)], [0,1,0])
 
-        src_data = src.read(1)
-        np_shape = (3, src_data.shape[0], src_data.shape[1])
-        suit_raster = np.zeros(np_shape, dtype=rio.int8)
-        # Reclassify to natural land cover
-        suit_raster[0] = np.select([src_data == 0, ((7 < src_data) & (src_data <= 12)) | ((13 < src_data) & (src_data <= 16)),\
-                                                    (src_data < 7) | (src_data == 13) | (src_data > 16)], [0,1,0])
-        # Reclassify to Agricultural land cover
-        suit_raster[1] = np.select([src_data == 0, (src_data == 13),\
-                                                    (src_data < 13) | (src_data > 13)], [0,1,0])
-        # Reclassify to Intensive
-        suit_raster[2] = np.select([src_data == 0, ((1 < src_data) & (src_data <= 7)),\
-                                                    (src_data < 1) | (src_data > 7)], [0,1,0])
+            profile = src.profile
+            profile.update(count=3)
+            # print(src.profile)
+            # print(profile)
 
-        profile = src.profile
-        profile.update(count=3)
-        # print(src.profile)
-        # print(profile)
+        with rio.open(out_path, "w", **profile) as dst:
+            dst.write(suit_raster)
 
-    with rio.open(out_path, "w", **profile) as dst:
-        dst.write(suit_raster)
+            dst.set_band_description(1, "NATURAL")
+            dst.set_band_description(2, "AGRICULTURAL")
+            dst.set_band_description(3, "INTENSIVE")
 
-        dst.set_band_description(1, "NATURAL")
-        dst.set_band_description(2, "AGRICULTURAL")
-        dst.set_band_description(3, "INTENSIVE")
+        del(src_data, suit_raster, src, dst)
 
-    del(src_data, suit_raster, src, dst)
+        print('{0}/{1} reclass saved. {2}_{3}_reclass.tif'.format(i+1, len(tif_paths), yr, sres))
 
-    print('{0}/{1} reclass saved. {2}_{3}_reclass.tif'.format(i+1, len(tif_paths), yr, sres))
+    else:
+        print(f'{yr}_{sres}_reclass.tif exists.')
 
 
 ##### Zonal Stats on reclass #####
@@ -102,57 +105,58 @@ for i in range(len(tif_paths)):
 print('\nStarting zonal stats.')
 tif_paths = glob('../data/LandCover/FORE-SCE/CONUS_Landcover_*/reclass/*.tif')
 
+src = rio.open(tif_paths[0])
+shp = gpd.read_file('../data/Shapefiles/HUC08/HUC08_paper2/HUC08_paper2.shp')
+huc8_reproj = shp.to_crs(crs=src.crs.to_dict()['init'])
+# point_reproj = pt_shp.to_crs(dataset.crs.to_dict())
+del(shp)
+huc8 = huc8_reproj[['huc8','geometry']]
+del(huc8_reproj)
+
 for i in range(len(tif_paths)):
     base_path = os.path.dirname(tif_paths[i])[:-7]
     yr = tif_paths[i].split('\\')[3].split('_')[0]
     sres = tif_paths[i].split('\\')[3].split('_')[1]
     
-    out_path = os.path.join(base_path, 'zonal_lclu_csv', '{0}_{1}_lclu.csv'.format(yr, sres))
+    out_path = os.path.join(base_path, 'zonal_lclu_csv', f'{yr}_{sres}_lclu.csv')
+    if not os.path.exists(out_path):
+        if not os.path.exists(os.path.dirname(out_path)):
+            os.makedirs(os.path.dirname(out_path))
 
-    src = rio.open(tif_paths[i])
+        src = rio.open(tif_paths[i])
 
-    if i == 0:
-        # src = rio.open(tif_paths[i])
-        shp = gpd.read_file('../data/Shapefiles/HUC08_trimmed.shp')
-        huc8_reproj = shp.to_crs(crs=src.crs.to_dict())
-        # point_reproj = pt_shp.to_crs(dataset.crs.to_dict())
-        del(shp)
-        huc8 = huc8_reproj[['huc8','geometry']]
-        del(huc8_reproj)
-    
-    if os.path.exists(out_path):
-        print('{0}_{1}_lclu.csv exists.'.format(yr, sres))
-        continue
+        huc8_df = pd.DataFrame(huc8['huc8'])
+        huc8_df['YEAR'] = int(yr)
+        huc8_df['PR_AG'] = 0
+        huc8_df['PR_NAT'] = 0
+        huc8_df['PR_INT'] = 0
 
-    huc8_df = pd.DataFrame(huc8['huc8'])
-    huc8_df['YEAR'] = int(yr)
-    huc8_df['PR_AG'] = 0
-    huc8_df['PR_NAT'] = 0
-    huc8_df['PR_INT'] = 0
+        affine = src.transform
+        for j in [1,2,3]:
+            array = src.read(j)
+            
+            zs = zonal_stats(huc8, array, affine=affine, stats=['sum', 'count'], nodata=-1, all_touched = False)
 
-    affine = src.transform
-    for j in [1,2,3]:
-        array = src.read(j)
+            zs_df = pd.DataFrame(zs)
+            zs_df = zs_df.fillna(0)
+
+            if j == 1:
+                var = 'PR_NAT'
+            elif j == 2:
+                var = 'PR_AG'
+            elif j == 3:
+                var = 'PR_INT'
+
+            huc8_df[var] += zs_df['sum'] / (zs_df['count'])
         
-        zs = zonal_stats(huc8, array, affine=affine, stats=['sum', 'count'], nodata=-1, all_touched = False)
+            del(array)
 
-        zs_df = pd.DataFrame(zs)
-        zs_df = zs_df.fillna(0)
+        src.close()
 
-        if j == 1:
-            var = 'PR_NAT'
-        elif j == 2:
-            var = 'PR_AG'
-        elif j == 3:
-            var = 'PR_INT'
+        huc8_df.to_csv(out_path, index=False)
+        print('{0}/{1} zonal stats saved. {2}_{3}_lclu.csv'.format(i+1, len(tif_paths), yr, sres))
 
-        huc8_df[var] += zs_df['sum'] / (zs_df['count'])
+        del(huc8_df)    
     
-        del(array)
-
-    src.close()
-
-    huc8_df.to_csv(out_path, index=False)
-    print('{0}/{1} zonal stats saved. {2}_{3}_lclu.csv'.format(i+1, len(tif_paths), yr, sres))
-
-    del(huc8_df)
+    else:
+        print('{yr}_{sres}_lclu.csv exists.'.format(yr, sres))
